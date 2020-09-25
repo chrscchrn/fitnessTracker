e13e95f4a94a7bdb6ecf8d73c67247f4399aead2
chrscchrn <cochranschris@gmail.com>
2020-09-24 22:43:40 -0500

    finishing functionality

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# fitnessTracker
+# Fitness Tracker
+Track all of your workouts with Fitness Tracker. This app will keep track of every exercise in your workout. The stats dashboard will show weekly summary graphs of all workouts done in a week.
+
+## User Story
+
+As a user, I want to be able to view create and track daily workouts. I want to be able to log multiple exercises in a workout on a given day. I should also be able to track the name, type, weight, sets, reps, and duration of exercise. If the exercise is a cardio exercise, I should be able to track my distance traveled.
+
+## Developed With
+HTML/CSS
+JavaScript
+NodeJS
+ExpressJS
+MongoDB
+Mongoose
+
+## Live Demo
+
+## Author
+
+## Screenshots
---
+++ routes/api-routes.js
@@ -0,0 +1,45 @@
+//db
+const db = require('../models')
+module.exports = (app) => {
+
+    //get all
+    app.get("/api/workouts", (req, res) => {
+        db.Workout.find({}, (err, workouts) => {
+            if(err){
+                console.log(err);
+            } else {
+                res.json(workouts)
+            }
+        });
+    });
+
+    //add excerise, set id, push to model, set true
+    app.put("/api/workouts/:workout", ({ params, body }, res) => {
+        db.Workout.findOneAndUpdate(
+            { _id: params.id},
+            {$push: {excercises:body }},
+            { upsert: true, useFindandModify:false},
+            updatedWorkout => {
+                res.json(updatedWorkout);
+            })
+    });
+
+    //create new workout
+    app.post('/api/workouts', (req,res) => {
+        db.Workout.create({}).then(newWorkout => {
+            res.json(newWorkout);
+        });
+    });
+
+    // get workouts in range
+    app.get("/api/workouts/range", (req, res) => {
+
+        db.Workout.find({}).then(dbWorkout => {
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.json(err);
+        });
+
+    });
+
+}
